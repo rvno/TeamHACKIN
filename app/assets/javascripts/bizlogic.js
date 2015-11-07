cbf3ce09a216daa15259c0652f9e25a6905faebb
Adam Fluke <fluke.adam@gmail.com>
2015-11-07 15:23:21 -0800

    include bizlogic js into the asset pipeline

--- app/assets/javascripts/bizlogic.js
+++ app/assets/javascripts/bizlogic.js
@@ -1,6 +1,35 @@
 $(document).ready(function(){
   var correctCharacters = $('#typingContent pre').text().split("");
   var characterIndex = 0;
+  var incorrectStrokes = {};
+
+  showStats = function(){
+    updateWastedStrokes();
+    updateTopMissedKeys();
+  };
+
+  updateWastedStrokes = function(){
+    var totalStrokes = 0;
+
+    for (var keyCode in incorrectStrokes) {
+      totalStrokes += incorrectStrokes[keyCode];
+      console.log(totalStrokes);
+    }
+
+    $('#stats span#strokes').text(totalStrokes);
+  }
+
+  updateTopMissedKeys = function(){
+
+  };
+
+  trackIncorrectStroke = function(keyCode){
+    incorrectStrokes[keyCode] = incorrectStrokes[keyCode] + 1 || 1;
+  };
+
+  highlightCurrent = function(){
+    highlightYellow(characterIndex);
+  };
 
   preparePage = function(){
     var highlightable = [];
@@ -9,21 +38,34 @@ $(document).ready(function(){
     });
 
     $('#typingContent pre').html(highlightable.join(""));
+    highlightCurrent();
   }
 
   highlightGreen = function(index){
-    $('span[data-character-index='+ index +']').addClass('green');
+    $('span[data-character-index='+ index +']').removeClass().addClass('green');
   };
+
+  highlightYellow = function(index){
+    $('span[data-character-index='+ index +']').removeClass().addClass('yellow');
+  }
+
+  highlightRed = function(index){
+    $('span[data-character-index='+ index +']').removeClass().addClass('red');
+  }
 
   checkKeyPress = function(keyCode){
     if(correctCharacters[characterIndex] === String.fromCharCode(keyCode)){
       highlightGreen(characterIndex);
       characterIndex += 1;
-    }
-
-    if(correctCharacters[characterIndex] === "\n" && keyCode === 13){
+      highlightCurrent();
+    }else if(correctCharacters[characterIndex] === "\n" && keyCode === 13){
       console.log('return entered');
       characterIndex += 1;
+      highlightCurrent();
+    } else {
+      trackIncorrectStroke(keyCode);
+      highlightRed(characterIndex);
+      showStats();
     }
   }
 
