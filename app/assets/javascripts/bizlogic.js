cf011b9710e00b3f34e68bcee00c6b1ca3da5f12
Adam Fluke <fluke.adam@gmail.com>
2015-11-07 14:16:23 -0800

    Add color class logic to functions

--- app/assets/javascripts/bizlogic.js
+++ app/assets/javascripts/bizlogic.js
@@ -2,10 +2,6 @@ $(document).ready(function(){
   var correctCharacters = $('#typingContent pre').text().split("");
   var characterIndex = 0;
 
-  highlightCurrent = function(){
-    highlightYellow(characterIndex);
-  };
-
   preparePage = function(){
     var highlightable = [];
     correctCharacters.forEach(function(value, index, a){
@@ -13,32 +9,21 @@ $(document).ready(function(){
     });
 
     $('#typingContent pre').html(highlightable.join(""));
-    highlightCurrent();
   }
 
   highlightGreen = function(index){
-    $('span[data-character-index='+ index +']').removeClass().addClass('green');
+    $('span[data-character-index='+ index +']').addClass('green');
   };
-
-  highlightYellow = function(index){
-    $('span[data-character-index='+ index +']').removeClass().addClass('yellow');
-  }
-
-  highlightRed = function(index){
-    $('span[data-character-index='+ index +']').removeClass().addClass('red');
-  }
 
   checkKeyPress = function(keyCode){
     if(correctCharacters[characterIndex] === String.fromCharCode(keyCode)){
       highlightGreen(characterIndex);
       characterIndex += 1;
-      highlightCurrent();
-    }else if(correctCharacters[characterIndex] === "\n" && keyCode === 13){
+    }
+
+    if(correctCharacters[characterIndex] === "\n" && keyCode === 13){
       console.log('return entered');
       characterIndex += 1;
-      highlightCurrent();
-    } else {
-      highlightRed(characterIndex);
     }
   }
 
