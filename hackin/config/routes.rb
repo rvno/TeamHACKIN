aec32a346b14f61947915a983feec777c001ccba
Chris Scott <christo247@gmail.com>
2015-11-07 11:11:11 -0800

    add splash page and show for type test

--- hackin/config/routes.rb
+++ hackin/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  resources 'welcome', only: [:show]
+  get '/type' => 'welcome#show'
   root 'welcome#index'
 
   # Example of regular route:
