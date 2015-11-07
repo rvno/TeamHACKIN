ec3192291a1ecae559b366f6d8f78b55b9aaaeed
Adam Fluke <fluke.adam@gmail.com>
2015-11-07 12:18:43 -0800

    add some basic string matching for typing characters

--- bizlogic.js
+++ bizlogic.js
@@ -1,3 +1,31 @@
 $(document).ready(function(){
-  alert('here');
+  var correctCharacters = $('#typingContent pre').text().split("");
+  var characterIndex = 0;
+
+  checkKeyPress = function(keyCode){
+    console.log('keyCode');
+    console.log(keyCode);
+    console.log('string entered');
+    console.log(String.fromCharCode(keyCode));
+    console.log('expected');
+    console.log(correctCharacters[characterIndex]);
+    console.log('array');
+    console.log(correctCharacters);
+    console.log('current index');
+    console.log(characterIndex);
+    if(correctCharacters[characterIndex] === String.fromCharCode(keyCode)){
+      console.log('correct letter');
+      characterIndex += 1;
+    }
+
+    if(correctCharacters[characterIndex] === "\n" && keyCode === 13){
+      console.log('return entered');
+      characterIndex += 1;
+    }
+  }
+
+  $("html").keypress(function(keyEvent){
+    checkKeyPress(keyEvent.keyCode);
+  })
+
 });
