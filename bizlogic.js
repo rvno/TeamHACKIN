1a32cf178ee768d5c97dffabbfbe0214659025b1
Jose Taveras <josejuanisaac@gmail.com>
2015-11-07 15:24:48 -0800

    UpdateCPM, characters per minute

--- bizlogic.js
+++ bizlogic.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   var correctCharacters = $('#typingContent pre').text().split("");
   var characterIndex = 0;
   var start_time = new Date();
+  var mistake = false;
   highlightCurrent = function(){
     highlightYellow(characterIndex);
   };
@@ -26,10 +27,12 @@ $(document).ready(function(){
 
   highlightRed = function(index){
     $('span[data-character-index='+ index +']').removeClass().addClass('red');
+    mistake = true;
   }
 
   checkKeyPress = function(keyCode){
-    var current_time = start_time
+    updateCPM();
+
     if(correctCharacters[characterIndex] === String.fromCharCode(keyCode)){
       highlightGreen(characterIndex);
       characterIndex += 1;
@@ -38,15 +41,38 @@ $(document).ready(function(){
       console.log('return entered');
       characterIndex += 1;
       highlightCurrent();
+    }else if(keyCode === 8){
+      if(mistake){
+
+        console.log('backspace entered');
+        highlightCurrent();
+        mistake = false;
+      }
     } else {
       highlightRed(characterIndex);
+
     }
   }
-
+  function updateCPM(){
+    //words typed
+      //there is a space between every 2 words, so basically every space represents one word. ish
+      // console.log(typeof correctCharacters);
+    var characters_typed = correctCharacters.join("").substr(0,characterIndex).length;
+    console.log(characters_typed);
+    //time
+    var time_spent = (new Date()).getTime() - start_time;
+    $('#cpm').html(characters_typed/(time_spent/60000));
+  }
   $("html").keypress(function(keyEvent){
     keyEvent.preventDefault();
     checkKeyPress(keyEvent.keyCode);
   })
-
+  $("html").keyup(function(keyEvent){
+    keyEvent.preventDefault();
+    // console.log(keyEvent.keyCode);
+    if(keyEvent.keyCode===8){
+      checkKeyPress(keyEvent.keyCode);
+    }
+  });
   preparePage();
 });
